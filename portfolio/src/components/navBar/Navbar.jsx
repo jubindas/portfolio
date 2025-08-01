e786c608ac3bf64cc6a36c8f8c3f3acb83349e30
jubindas <jubinrohidas78@gmail.com>
2025-08-01 21:09:30 +0530

    idk what i did

--- portfolio/src/components/navBar/Navbar.jsx
+++ portfolio/src/components/navBar/Navbar.jsx
@@ -1,24 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import './navBar.css';
-import { FaSun, FaMoon } from 'react-icons/fa';
+import React, { useState } from "react";
+import "./navBar.css";
+import { FaSun, FaMoon, FaBars, FaTimes } from "react-icons/fa";
 
 function Navbar({ darkMode, setDarkMode }) {
- 
+  const [menuOpen, setMenuOpen] = useState(false);
 
-
+  const toggleMenu = () => setMenuOpen(!menuOpen);
 
   return (
     <nav className="custom-navbar">
       <div className="logo">Jubin</div>
 
-     
+      {/* Hamburger Icon */}
+      <div className="hamburger" onClick={toggleMenu}>
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </div>
 
       {/* Navigation links */}
-      <div className={`nav-links show-menu`}>
-        <a href="#about" >About</a>
-        <a href="#skills" >Skills</a>
-        <a href="#projects" >Projects</a>
-        <a href="#contact" >Contact</a>
+      <div className={`nav-links ${menuOpen ? "show-menu" : ""}`}>
+        <a href="#about" onClick={() => setMenuOpen(false)}>
+          About
+        </a>
+        <a href="#skills" onClick={() => setMenuOpen(false)}>
+          Skills
+        </a>
+        <a href="#projects" onClick={() => setMenuOpen(false)}>
+          Projects
+        </a>
+        <a href="#contact" onClick={() => setMenuOpen(false)}>
+          Contact
+        </a>
         <button className="mode-toggle" onClick={() => setDarkMode(!darkMode)}>
           {darkMode ? <FaSun /> : <FaMoon />}
         </button>
